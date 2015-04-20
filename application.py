c930420243307237c3ae93865dff0cca81d39717
Gerald Varghese <gerald.varghese@gmail.com>
2015-04-19 23:02:15 -0500

    included tweepy

--- application.py
+++ application.py
@@ -5,6 +5,7 @@
 """
 
 import flask, flask.views
+import tweepy
 
 #import os
 
@@ -22,6 +23,32 @@ application = flask.Flask(__name__)
 application.secret_key = "bacon"
 
 
+
+
+
+class ShowTweets(flask.views.MethodView):
+    def get(self):
+        consumer_key = "mpIuWJYkQKUvaiS4FPwQpGVr8"
+        consumer_secret = "EWOz9A9om3tf85XsF89KbIVC5LUkHEZNhdy2PcHTfOr9tP4jjE"
+        access_token = "3080403725-gleW4H38K4tJ69vtUFJDZgBCr2VtqFb3D06Xk7y"
+        access_token_secret = "zWxk43qe3c8QlP6Pua2A81UvDTlpe90lqUVC5PxZEzcqg"
+        tweets= []
+        
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        
+        api = tweepy.API(auth)
+        
+        public_tweets = api.home_timeline()
+        for tweet in public_tweets:
+            #tweets.append(tweet.text)
+            #hashtags = [hashtag.text for hashtag in tweet.entities.hashtags]
+            #print(hashtags)
+            tweets.append([tweet.text,tweet.user.screen_name,tweet.favorite_count])
+        print(tweets)
+        return flask.render_template('show_tweets.html', tweets=tweets)
+
+
 #routes
 application.add_url_rule('/',
                  view_func=Login.as_view('login'),
@@ -34,6 +61,9 @@ application.add_url_rule('/register/',
                  methods=['GET','POST'])
 application.add_url_rule('/result/',
                  view_func=Result.as_view('result'), 
+                 methods=['GET'])
+application.add_url_rule('/showtweets',
+                 view_func=ShowTweets.as_view('showtweets'),
                  methods=['GET'])
 
 #handler to page not found - or incorrect URL
